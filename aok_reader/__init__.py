7a3965eacf12e2982105db58715e00f146556417
ryanamannion <ram321@georgetown.edu>
2020-10-29 19:53:36 -0400

    implements loading of master csv

---
+++ aok_reader/__init__.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python3
+"""
+aok_reader/__init__.py
+
+By Ryan A. Mannion
+ram321@georgetown.edu
+twitter @ryanamannion
+
+A package to manage reading and loading the AOK dataset into python objects
+
+Assumes the top-level dir is as initially downloaded
+
+By "as initially downloaded" I mean when you unarchive the zip file, direct the
+reader to the resulting directory. As of the beta, this includes:
+    - full_contracts/     # contains full contract pdf files
+    - individual_contract_clauses/    # contains relevant clauses
+    - master_clauses.csv
+    and some other information about the corpus
+
+TODO: Add usage
+
+"""
+
+import csv
+import glob
+import ft
+import sys
+
+
+class Reader:
+    """
+    Handles reading of the corpus
+
+    Attributes:
+        path: str, path to top-level directory of AOK as downloaded
+
+    """
+
+    def __init__(self, path):
+        """
+        Initialize with path to top-level dir
+
+        :param path: str, path to top-level AOK dir
+        """
+        if not path.endswith('/'):
+            path = path + '/'
+        self.path = path
+
+    def load_master_csv(self):
+        """
+        load master csv
+
+        :param path: str, path to top-level corpus directory
+        :param generator: bool, if True loads master csv as a generator, else
+            load it all at once
+        :return: TODO
+        """
+
+        path_to_master_csv = self.path + 'master_clauses.csv'
+
+        entire_sheet = []
+        with open(path_to_master_csv, 'r') as fstream:
+            rows = csv.reader(fstream, delimiter=',', quotechar='"')
+            for i, row in enumerate(rows):
+                if i == 0:
+                    headers = row       # store headers
+                    continue
+                else:       # make a free table of the master csv
+                    # want to change some values to make it more python friendly
+                    new_row = []
+                    for cell in row:
+                        if cell == '':
+                            new_row.append(None)
+                        elif '\n\n' in cell:
+                            # multiple items, split and store as list
+                            new_row.append(cell.split('\n\n'))
+                        elif cell.lower() == 'yes':
+                            new_row.append(True)
+                        elif cell.lower() == 'no':
+                            new_row.append(False)
+                        else:
+                            new_row.append(cell)
+
+                    doc = dict(zip(headers, new_row))
+                    entire_sheet.append(doc)
+
+        return entire_sheet
